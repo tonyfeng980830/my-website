52fded1ed6e5ed3c3e103f637070b64ae8fb04d6
tonyfeng980830 <tonyfeng@connect.hku.hk>
2024-01-02 23:24:44 -0500

    updated bug

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -151,23 +151,6 @@ const Homepage = () => {
 						</div>
 
 						<div className="homepage-after-title">
-							{/* <div className="homepage-articles">
-								{myArticles.map((article, index) => (
-									<div
-										className="homepage-article"
-										key={(index + 1).toString()}
-									>
-										<Article
-											key={(index + 1).toString()}
-											date={article().date}
-											title={article().title}
-											description={article().description}
-											link={"/article/" + (index + 1)}
-										/>
-									</div>
-								))}
-							</div> */}
-
 							<div className="homepage-works">
 								<Works />
 							</div>
